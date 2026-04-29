9b2d35a5e7c527b52517f426c7edc8c1decc5d9b
Nael-Emin Ben Oun <benounnaelemin@gmail.com>
2026-04-29 16:41:53 +0200

    fixed nav bar items and added new version label

--- prisma/backup.ts
+++ prisma/backup.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import 'dotenv/config'
 import fs from 'node:fs/promises'
 import path from 'node:path'
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import 'dotenv/config'
 import { PrismaClient } from '../src/generated/prisma/client'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
